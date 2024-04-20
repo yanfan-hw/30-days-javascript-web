2224ce9d16dc8b7612c8b0593aaeda065fe0b928
YanFan Hw <>
2024-04-20 22:26:31 +0700

    [01] - day

---
+++ day-01/app.js
@@ -0,0 +1,24 @@
+/*
+ * 30 Day Javascript Web
+ */
+
+//todo variable js
+const string = 'Hello World';
+const boolean = true;
+const _undefined = undefined;
+const _null = null;
+
+//todo datatype js
+console.log('Type of variable: ', typeof(string));
+console.log('Type of variable: ', typeof(boolean));
+console.log('Type of variable: ', typeof(_undefined));
+console.log('Type of variable: ', typeof(_null));
+
+//todo Information
+const myFirstName = 'Huỳnh';
+const myLastName = 'Hưng Hải';
+const maritalStatus = 'Married';
+const country = 'Viet Nam';
+const age = '28';
+
+console.log(`I am ${myLastName} ${myFirstName}.\nI am ${maritalStatus}.\nI am living in ${country}.\nI am ${age} years old.`);
